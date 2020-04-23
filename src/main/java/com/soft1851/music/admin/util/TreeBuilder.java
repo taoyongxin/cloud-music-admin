08c7b99bed3ea5bb7f4428b39292f795e27ae7c8
tyx <1427177855@qq.com>
2020-04-23 20:41:29 +0800

    修正登录

---
+++ src/main/java/com/soft1851/music/admin/util/TreeBuilder.java
@@ -0,0 +1,11 @@
+package com.soft1851.music.admin.util;
+
+/**
+ * @author Tao
+ * @version 1.0
+ * @ClassName TreeBuilder
+ * @Description TODO
+ * @date 2020-04-23 19:31
+ **/
+public class TreeBuilder {
+}
---
+++ src/test/java/com/soft1851/music/admin/service/SysRoleServiceTest.java
@@ -0,0 +1,12 @@
+package com.soft1851.music.admin.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SysRoleServiceTest {
+
+    @Test
+    void selectRoleById() {
+    }
+}
